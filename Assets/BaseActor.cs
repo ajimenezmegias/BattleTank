11a62278b546aa23e942dd08f47329e223b24e39
ajimenez <ajimenezmegias@gmail.com>
2018-02-18 22:09:12 +0100

    Base Project

---
+++ Assets/BaseActor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseActor
+{
+	public int HP;
+
+	public Position Position;
+	public Direction Direction;
+
+	/// <summary>
+	/// Speed as "cooldown" to change tile
+	/// </summary>
+	public float Speed;
+}
+
+public class GameController
+{
+    private List<BaseActor> Actors;
+    private  BaseActor[,] Grid;
+}
+
+public class GameBoard
+{
+    
+}
---
+++ Assets/Position.cs
@@ -0,0 +1,5 @@
+﻿public class Position
+{
+	int X;
+	int Y;
+}
